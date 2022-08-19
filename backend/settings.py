f6226dffda2bc966e8ec7c9772c88d6d344d4224
klauvital <klauvital@gmail.com>
2022-08-18 23:43:42 -0300

    fix: Corrige databases em settings

--- backend/settings.py
+++ backend/settings.py
@@ -74,7 +74,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -85,18 +84,7 @@ DATABASES = {
         'PORT': config('DB_PORT', 5432),
     }
 }
-'''
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'SGWC',
-        'USER': 'postgres',
-        'PASSWORD': 'nathan09',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
 
 
 # Email config
